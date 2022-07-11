20b243ce7f19fb14a2a1a81ee4462ef069bb61e3
yejinie <smoytn3@naver.com>
2022-07-11 18:08:34 +0900

    add : [Session] 밴드 수정, 삭제 기능 추가

--- src/main/java/com/example/demo/src/session/SessionDao.java
+++ src/main/java/com/example/demo/src/session/SessionDao.java
@@ -1,5 +1,7 @@
 package com.example.demo.src.session;
 
+import com.example.demo.src.pofol.model.PatchPofolReq;
+import com.example.demo.src.session.model.PatchBandReq;
 import com.example.demo.src.session.model.PostBandReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,6 +18,26 @@ public class SessionDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    // 유저 확인
+    public int checkUserExist(int userIdx){
+        String checkUserExistQuery = "select exists(select userIdx from User where userIdx = ?)";
+        int checkUserExistParams = userIdx;
+        return this.jdbcTemplate.queryForObject(checkUserExistQuery,
+                                                int.class,
+                                                checkUserExistParams);
+
+    }
+
+    // 밴드 확인
+    public int checkBandExist(int bandIdx){
+        String checkBandExistQuery = "select exists(select bandIdx from Band where bandIdx = ?)";
+        int checkBandExistParams = bandIdx;
+        return this.jdbcTemplate.queryForObject(checkBandExistQuery,
+                                                int.class,
+                                                checkBandExistParams);
+
+    }
+
     // 밴드 생성
     public int insertBand(int userIdx, PostBandReq postBandReq){
         String insertBandQuery = "INSERT INTO Band(userIdx, bandTitle, bandIntroduction, bandRegion, bandContent, vocal, guitar, base, keyboard, drum, bandImgUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -28,5 +50,21 @@ public class SessionDao {
         String lastInsertIdQuery = "select last_insert_id()";
         return this.jdbcTemplate.queryForObject(lastInsertIdQuery, int.class);
 
+    }
+
+    // 밴드 수정
+    public int updateBand(int bandIdx, PatchBandReq patchBandReq){
+        String updateBandQuery = "UPDATE Band SET bandContent = ? WHERE bandIdx = ?" ;
+        Object[] updateBandParams = new Object[]{patchBandReq.getBandContent(), bandIdx};
+
+        return this.jdbcTemplate.update(updateBandQuery,updateBandParams);
+    }
+
+    // 밴드 삭제
+    public int updateBandStatus(int bandIdx){
+        String deleteBandQuery = "UPDATE Band SET status = 'INACTIVE' WHERE bandIdx = ? ";
+        Object[] deleteBandParams = new Object[]{bandIdx};
+
+        return this.jdbcTemplate.update(deleteBandQuery,deleteBandParams);
     }
 }
