0fdbbffbf6ac67604f377be0c43cf180d3d73280
JIYOUNG PARK <ziy913@naver.com>
2022-07-16 01:12:40 +0900

    add: [S33] S3 이미지 업로드

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
+    //S3
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
+
     public static void main(String[] args) {
+
+
 
         SpringApplication.run(DemoApplication.class, args);
 
@@ -15,4 +26,9 @@ public class DemoApplication {
         System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
     }
 
+
+
+
 }
+
+
