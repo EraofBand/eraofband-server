5f1176bf0783b4d0c6e11389fb6414ea3a175cce
JIYOUNG PARK <ziy913@naver.com>
2022-07-16 03:02:51 +0900

    제발

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,12 +14,20 @@ public class DemoApplication {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
     }
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "classpath:aws.yml";
+
 
     public static void main(String[] args) {
 
+        new SpringApplicationBuilder(DemoApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
 
 
-        SpringApplication.run(DemoApplication.class, args);
+
+        //SpringApplication.run(DemoApplication.class, args);
 
         // 메모리 사용량 출력
         long heapSize = Runtime.getRuntime().totalMemory();
