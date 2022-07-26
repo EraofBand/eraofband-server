9d654da8943c7aaa648213cbb67a84972e820419
yejinie <smoytn3@naver.com>
2022-07-26 14:43:08 +0900

    add: [Lesson] API 설명 추가

--- src/main/java/com/example/demo/src/lesson/model/PostLessonReq.java
+++ src/main/java/com/example/demo/src/lesson/model/PostLessonReq.java
@@ -1,5 +1,6 @@
 package com.example.demo.src.lesson.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +14,15 @@ public class PostLessonReq {
 
     private String lessonTitle;
 
+    @ApiModelProperty(example="레슨 한 줄 소개")
     private String lessonIntroduction;
 
     private String lessonRegion;
 
+    @ApiModelProperty(example="레슨 소개 내용")
     private String lessonContent;
 
+    @ApiModelProperty(example="레슨 모집 세션")
     private int lessonSession;
 
     //private int mySession;
@@ -27,5 +31,6 @@ public class PostLessonReq {
 
     private String chatRoomLink;
 
+    @ApiModelProperty(example="레슨 모집 이미지 URL")
     private String lessonImgUrl;
 }
