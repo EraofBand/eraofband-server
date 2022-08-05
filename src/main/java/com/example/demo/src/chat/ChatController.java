388394e91df3d45b25996dd092354f87116b7482
yejinie <smoytn3@naver.com>
2022-08-05 20:36:58 +0900

    add: [chat] 채팅방 리스트 조회

--- src/main/java/com/example/demo/src/chat/ChatController.java
+++ src/main/java/com/example/demo/src/chat/ChatController.java
@@ -2,6 +2,7 @@ package com.example.demo.src.chat;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
+import com.example.demo.src.chat.model.GetChatRoomRes;
 import com.example.demo.utils.JwtService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +10,8 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.example.demo.config.BaseResponseStatus.INVALID_USER_JWT;
 
@@ -29,9 +32,29 @@ public class ChatController {
         this.jwtService = jwtService;
     }
 
+    /**
+     * 채팅방 리스트 조회 API
+     * [GET] /chat
+     * @return BaseResponse<List<GetChatRoomRes>>
+     */
+    @ResponseBody
+    @GetMapping("/") // (get) https://eraofband.shop/chat
+    @ApiOperation(value = "채팅방 리스트 조회", notes = "헤더에 jwt 필요(key: X-ACCESS-TOKEN, value: jwt 값)")
+    @ApiResponses({
+            @ApiResponse(code=4000, message="데이터베이스 연결에 실패하였습니다.")
+    })
+    public BaseResponse<List<GetChatRoomRes>> getChatRoom(){
+        try{
+            //jwt에서 idx 추출.
+            int userIdxByJwt = jwtService.getUserIdx();
 
+            List<GetChatRoomRes> getChatRoomRes = chatProvider.getChatRoom(userIdxByJwt);
+            return new BaseResponse<>(getChatRoomRes);
 
-
+        } catch (BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
 
     /**
      * 채팅방 나가기 API
@@ -39,7 +62,7 @@ public class ChatController {
      * @return BaseResponse<String>
      */
     @ResponseBody
-    @PatchMapping("/status/{chatRoomIdx}") // (patch) https://eraofband.shop/lessons/status/2
+    @PatchMapping("/status/{chatRoomIdx}") // (patch) https://eraofband.shop/chat/status/2
     @ApiOperation(value = "레슨 삭제 처리", notes = "헤더에 jwt 필요(key: X-ACCESS-TOKEN, value: jwt 값)")
     @ApiImplicitParam(name="chatRoomIdx", value="나갈 채팅방 인덱스", required = true)
     @ApiResponses({
