38267b0de2b1157d3c7df69b9be1c2e884a20980
JIYOUNG PARK <ziy913@naver.com>
2022-08-08 00:35:58 +0900

    add: [Pofol] 피드 조회(페이징)

--- src/main/java/com/example/demo/src/pofol/PofolDao.java
+++ src/main/java/com/example/demo/src/pofol/PofolDao.java
@@ -107,45 +107,184 @@ public class PofolDao {
                         rs.getString("updatedAt")
                 ), selectCommentPofolParam);
 
+    }
 
+    /**
+     * 모든 포트폴리오 리스트 조회
+     * */
+    public List<GetPofolRes> selectAllPofol(int userIdx, int pofolIdx) {
+
+        String selectAllUserPofolQuery = "";
+        Object[] selectAllUserPofolParam = new Object[]{};
+
+        if(pofolIdx!=0){
+
+            selectAllUserPofolQuery = "\n" +
+                    " SELECT p.pofolIdx as pofolIdx,\n" +
+                    "                            u.userIdx as userIdx,\n" +
+                    "                            u.nickName as nickName,\n" +
+                    "                            u.profileImgUrl as profileImgUrl,\n" +
+                    "                            p.title as title,\n" +
+                    "                            p.content as content,\n" +
+                    "                            p.videoUrl as videoUrl,\n" +
+                    "                            IF(pofolLikeCount is null, 0, pofolLikeCount) as pofolLikeCount,\n" +
+                    "                            IF(commentCount is null, 0, commentCount) as commentCount,\n" +
+                    "                            case\n" +
+                    "                                when timestampdiff(second, p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')\n" +
+                    "                                when timestampdiff(minute , p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')\n" +
+                    "                                when timestampdiff(hour , p.updatedAt, current_timestamp) < 24\n" +
+                    "                                    then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')\n" +
+                    "                                when timestampdiff(day , p.updatedAt, current_timestamp) < 365\n" +
+                    "                                    then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')\n" +
+                    "                                else timestampdiff(year , p.updatedAt, current_timestamp)\n" +
+                    "                            end as updatedAt,\n" +
+                    "                            IF(pl.status = 'ACTIVE', 'Y', 'N') as likeOrNot\n" +
+                    "                        FROM Pofol as p\n" +
+                    "                            join User as u on u.userIdx = p.userIdx\n" +
+                    "                            left join (select pofolIdx, userIdx, count(pofolLikeIdx) as pofolLikeCount from PofolLike WHERE status = 'ACTIVE' group by pofolIdx) plc on plc.pofolIdx = p.pofolIdx\n" +
+                    "                            left join (select pofolIdx, count(pofolCommentIdx) as commentCount from PofolComment WHERE status = 'ACTIVE' group by pofolIdx) c on c.pofolIdx = p.pofolIdx\n" +
+                    "                            left join PofolLike as pl on pl.userIdx = ? and pl.pofolIdx = p.pofolIdx\n" +
+                    "                        WHERE p.status = 'ACTIVE' and p.pofolIdx<?\n" +
+                    "                        group by p.pofolIdx order by p.pofolIdx DESC\n" +
+                    "LIMIT 20;";
+
+            selectAllUserPofolParam = new Object[]{userIdx, pofolIdx};
+        }
+        else{
+            selectAllUserPofolQuery = "\n" +
+                    " SELECT p.pofolIdx as pofolIdx,\n" +
+                    "                            u.userIdx as userIdx,\n" +
+                    "                            u.nickName as nickName,\n" +
+                    "                            u.profileImgUrl as profileImgUrl,\n" +
+                    "                            p.title as title,\n" +
+                    "                            p.content as content,\n" +
+                    "                            p.videoUrl as videoUrl,\n" +
+                    "                            IF(pofolLikeCount is null, 0, pofolLikeCount) as pofolLikeCount,\n" +
+                    "                            IF(commentCount is null, 0, commentCount) as commentCount,\n" +
+                    "                            case\n" +
+                    "                                when timestampdiff(second, p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')\n" +
+                    "                                when timestampdiff(minute , p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')\n" +
+                    "                                when timestampdiff(hour , p.updatedAt, current_timestamp) < 24\n" +
+                    "                                    then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')\n" +
+                    "                                when timestampdiff(day , p.updatedAt, current_timestamp) < 365\n" +
+                    "                                    then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')\n" +
+                    "                                else timestampdiff(year , p.updatedAt, current_timestamp)\n" +
+                    "                            end as updatedAt,\n" +
+                    "                            IF(pl.status = 'ACTIVE', 'Y', 'N') as likeOrNot\n" +
+                    "                        FROM Pofol as p\n" +
+                    "                            join User as u on u.userIdx = p.userIdx\n" +
+                    "                            left join (select pofolIdx, userIdx, count(pofolLikeIdx) as pofolLikeCount from PofolLike WHERE status = 'ACTIVE' group by pofolIdx) plc on plc.pofolIdx = p.pofolIdx\n" +
+                    "                            left join (select pofolIdx, count(pofolCommentIdx) as commentCount from PofolComment WHERE status = 'ACTIVE' group by pofolIdx) c on c.pofolIdx = p.pofolIdx\n" +
+                    "                            left join PofolLike as pl on pl.userIdx = ? and pl.pofolIdx = p.pofolIdx\n" +
+                    "                        WHERE p.status = 'ACTIVE'\n" +
+                    "                        group by p.pofolIdx order by p.pofolIdx DESC\n" +
+                    "LIMIT 20;\n";
+
+            selectAllUserPofolParam = new Object[]{userIdx};
+
+        }
+
+        return this.jdbcTemplate.query(selectAllUserPofolQuery,
+                (rs, rowNum) -> new GetPofolRes(
+                        rs.getInt("pofolIdx"),
+                        rs.getInt("userIdx"),
+                        rs.getString("nickName"),
+                        rs.getString("profileImgUrl"),
+                        rs.getString("title"),
+                        rs.getString( "content"),
+                        rs.getInt("pofolLikeCount"),
+                        rs.getInt("commentCount"),
+                        rs.getString("updatedAt"),
+                        rs.getString("likeOrNot"),
+                        rs.getString("videoUrl")
+                ), selectAllUserPofolParam);
 
     }
 
     /**
      * 팔로우 한 유저 포트폴리오 리스트 조회
      * */
-    public List<GetPofolRes> selectPofol(int userIdx) {
-        String selectUserPofolQuery = "\n" +
-                "        SELECT p.pofolIdx as pofolIdx,\n" +
-                "            u.userIdx as userIdx,\n" +
-                "            u.nickName as nickName,\n" +
-                "            u.profileImgUrl as profileImgUrl,\n" +
-                "            p.title as title,\n" +
-                "            p.content as content,\n" +
-                "            p.videoUrl as videoUrl,\n" +
-                "            IF(pofolLikeCount is null, 0, pofolLikeCount) as pofolLikeCount,\n" +
-                "            IF(commentCount is null, 0, commentCount) as commentCount,\n" +
-                "            case\n" +
-                "                when timestampdiff(second, p.updatedAt, current_timestamp) < 60\n" +
-                "                    then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')\n" +
-                "                when timestampdiff(minute , p.updatedAt, current_timestamp) < 60\n" +
-                "                    then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')\n" +
-                "                when timestampdiff(hour , p.updatedAt, current_timestamp) < 24\n" +
-                "                    then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')\n" +
-                "                when timestampdiff(day , p.updatedAt, current_timestamp) < 365\n" +
-                "                    then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')\n" +
-                "                else timestampdiff(year , p.updatedAt, current_timestamp)\n" +
-                "            end as updatedAt,\n" +
-                "            IF(pl.status = 'ACTIVE', 'Y', 'N') as likeOrNot\n" +
-                "        FROM Pofol as p\n" +
-                "            join User as u on u.userIdx = p.userIdx\n" +
-                "            left join (select pofolIdx, userIdx, count(pofolLikeIdx) as pofolLikeCount from PofolLike WHERE status = 'ACTIVE' group by pofolIdx) plc on plc.pofolIdx = p.pofolIdx\n" +
-                "            left join (select pofolIdx, count(pofolCommentIdx) as commentCount from PofolComment WHERE status = 'ACTIVE' group by pofolIdx) c on c.pofolIdx = p.pofolIdx\n" +
-                "            left join Follow as f on f.followeeIdx = p.userIdx and f.status = 'ACTIVE'\n" +
-                "            left join PofolLike as pl on pl.userIdx = f.followerIdx and pl.pofolIdx = p.pofolIdx\n" +
-                "        WHERE f.followerIdx = ? and p.status = 'ACTIVE'\n" +
-                "        group by p.pofolIdx order by p.pofolIdx DESC; \n";
-        int selectUserPofolParam = userIdx;
+    public List<GetPofolRes> selectPofol(int userIdx, int pofolIdx) {
+
+        String selectUserPofolQuery = "";
+        Object[] selectUserPofolParam = new Object[]{};
+
+        if(pofolIdx!=0){
+
+            selectUserPofolQuery = "\n" +
+                    "         SELECT p.pofolIdx as pofolIdx,\n" +
+                    "                            u.userIdx as userIdx,\n" +
+                    "                            u.nickName as nickName,\n" +
+                    "                            u.profileImgUrl as profileImgUrl,\n" +
+                    "                            p.title as title,\n" +
+                    "                            p.content as content,\n" +
+                    "                            p.videoUrl as videoUrl,\n" +
+                    "                            IF(pofolLikeCount is null, 0, pofolLikeCount) as pofolLikeCount,\n" +
+                    "                            IF(commentCount is null, 0, commentCount) as commentCount,\n" +
+                    "                            case\n" +
+                    "                                when timestampdiff(second, p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')\n" +
+                    "                                when timestampdiff(minute , p.updatedAt, current_timestamp) < 60\n" +
+                    "                                    then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')\n" +
+                    "                                when timestampdiff(hour , p.updatedAt, current_timestamp) < 24\n" +
+                    "                                    then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')\n" +
+                    "                                when timestampdiff(day , p.updatedAt, current_timestamp) < 365\n" +
+                    "                                    then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')\n" +
+                    "                                else timestampdiff(year , p.updatedAt, current_timestamp)\n" +
+                    "                            end as updatedAt,\n" +
+                    "                            IF(pl.status = 'ACTIVE', 'Y', 'N') as likeOrNot\n" +
+                    "                        FROM Pofol as p\n" +
+                    "                            join User as u on u.userIdx = p.userIdx\n" +
+                    "                            left join (select pofolIdx, userIdx, count(pofolLikeIdx) as pofolLikeCount from PofolLike WHERE status = 'ACTIVE' group by pofolIdx) plc on plc.pofolIdx = p.pofolIdx\n" +
+                    "                            left join (select pofolIdx, count(pofolCommentIdx) as commentCount from PofolComment WHERE status = 'ACTIVE' group by pofolIdx) c on c.pofolIdx = p.pofolIdx\n" +
+                    "                            left join Follow as f on f.followeeIdx = p.userIdx and f.status = 'ACTIVE'\n" +
+                    "                            left join PofolLike as pl on pl.userIdx = f.followerIdx and pl.pofolIdx = p.pofolIdx\n" +
+                    "                        WHERE f.followerIdx = ? and p.status = 'ACTIVE' and p.pofolIdx<?\n" +
+                    "                        group by p.pofolIdx order by p.pofolIdx DESC\n" +
+                    "LIMIT 20;";
+
+            selectUserPofolParam = new Object[]{userIdx, pofolIdx};
+        }
+        else{
+            selectUserPofolQuery = "\n" +
+                    "        SELECT p.pofolIdx as pofolIdx,\n" +
+                    "            u.userIdx as userIdx,\n" +
+                    "            u.nickName as nickName,\n" +
+                    "            u.profileImgUrl as profileImgUrl,\n" +
+                    "            p.title as title,\n" +
+                    "            p.content as content,\n" +
+                    "            p.videoUrl as videoUrl,\n" +
+                    "            IF(pofolLikeCount is null, 0, pofolLikeCount) as pofolLikeCount,\n" +
+                    "            IF(commentCount is null, 0, commentCount) as commentCount,\n" +
+                    "            case\n" +
+                    "                when timestampdiff(second, p.updatedAt, current_timestamp) < 60\n" +
+                    "                    then concat(timestampdiff(second, p.updatedAt, current_timestamp), '초 전')\n" +
+                    "                when timestampdiff(minute , p.updatedAt, current_timestamp) < 60\n" +
+                    "                    then concat(timestampdiff(minute, p.updatedAt, current_timestamp), '분 전')\n" +
+                    "                when timestampdiff(hour , p.updatedAt, current_timestamp) < 24\n" +
+                    "                    then concat(timestampdiff(hour, p.updatedAt, current_timestamp), '시간 전')\n" +
+                    "                when timestampdiff(day , p.updatedAt, current_timestamp) < 365\n" +
+                    "                    then concat(timestampdiff(day, p.updatedAt, current_timestamp), '일 전')\n" +
+                    "                else timestampdiff(year , p.updatedAt, current_timestamp)\n" +
+                    "            end as updatedAt,\n" +
+                    "            IF(pl.status = 'ACTIVE', 'Y', 'N') as likeOrNot\n" +
+                    "        FROM Pofol as p\n" +
+                    "            join User as u on u.userIdx = p.userIdx\n" +
+                    "            left join (select pofolIdx, userIdx, count(pofolLikeIdx) as pofolLikeCount from PofolLike WHERE status = 'ACTIVE' group by pofolIdx) plc on plc.pofolIdx = p.pofolIdx\n" +
+                    "            left join (select pofolIdx, count(pofolCommentIdx) as commentCount from PofolComment WHERE status = 'ACTIVE' group by pofolIdx) c on c.pofolIdx = p.pofolIdx\n" +
+                    "            left join Follow as f on f.followeeIdx = p.userIdx and f.status = 'ACTIVE'\n" +
+                    "            left join PofolLike as pl on pl.userIdx = f.followerIdx and pl.pofolIdx = p.pofolIdx\n" +
+                    "        WHERE f.followerIdx = ? and p.status = 'ACTIVE'\n" +
+                    "        group by p.pofolIdx order by p.pofolIdx DESC LIMIT 20; \n";
+
+            selectUserPofolParam = new Object[]{userIdx};
+
+        }
+
         return this.jdbcTemplate.query(selectUserPofolQuery,
                 (rs, rowNum) -> new GetPofolRes(
                         rs.getInt("pofolIdx"),
