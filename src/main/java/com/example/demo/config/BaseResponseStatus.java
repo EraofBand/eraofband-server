dfcdbaa31372ddee45864526fc53aabba16ae42b
yejinie <smoytn3@naver.com>
2022-08-10 23:05:16 +0900

    add: [Board] 게시물 추천 기능 추가

--- src/main/java/com/example/demo/config/BaseResponseStatus.java
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -104,7 +104,7 @@ public enum BaseResponseStatus {
     ADD_FAIL_BOARD(false,2104,"게시글 조회 수 증가에 실패했습니다."),
     POSTS_EMPTY_BOARD_COMMENT_ID(false,2105,"게시글 댓글 아이디 값을 확인해주세요."),
     DELETE_FAIL_BOARD_COMMENT(false,2106,"게시글 댓글 삭제에 실패했습니다."),
-
+    DUPLICATED_BOARD_LIKE(false,2107,"이미 추천한 게시글입니다."),
 
 
 
